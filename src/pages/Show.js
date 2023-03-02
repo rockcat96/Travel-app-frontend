7349636c5c2632ca3250233eaee732c471651f65
Michelle Chen <chensiqi96@gmail.com>
2023-03-01 17:29:18 -0800

    adds styling to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,43 +1,44 @@
-import { Link, useLoaderData, Form } from "react-router-dom";
+import {useLoaderData, Form } from "react-router-dom";
 
 // destructuring the props needed to get our post, including router prop match
 const Show = (props) => {
   const post = useLoaderData();
 
-  ////////////////////
-  // Styles
-  ///////////////////
-  const div = {
-    textAlign: "center",
-    border: "3px solid green",
-    width: "80%",
-    margin: "30px auto",
-  };
 
   return (
-    <div style={div}>
-      <h1>{post.name}</h1>
-      <h2>{post.cities}</h2>
-      <h2>{post.month}</h2>
-      <h2>{post.days}</h2>
-      <img src={post.url} alt={post.name} width="500" height="600"/>
-      <div style={{ textAlign: "center" }}>
+    <div className="show">
+
+      <div className="show_img">
+        <img src={post.url} alt={post.name} width="500" height="600"/>
+      </div>
+      
+      <div className="show_country">
+        <h1>{post.name}</h1>
+        <h2><span className="bold">Cities: </span>{post.cities}</h2>
+        <h2><span className="bold">Best time to visit: </span>{post.month}</h2>
+        <h2><span className="bold">Number of days to visit: </span>{post.days}</h2>
+      </div>
+      
+      <div className="update_form">
+
         <h2>Update this Country</h2>
-        <Form action={`/update/${post.id}`} method="post">
+
+        <Form action={`/update/${post.id}`} method="post" className="form">
           <input type="text" name="name" placeholder="Country name" defaultValue={post.name}/>
-          <input type="text" name="cities" placeholder="Cities that you want to visit" defaultValue={post.cities}/>
+          <input type="text" name="cities" placeholder="Cities that you want to visit" 
+          defaultValue={post.cities}/>
           <input type="text" name="month" placeholder="The ideal month/s to visit this country" defaultValue={post.month}/>
           <input type="text" name="days" placeholder="Number of days to visit this country" defaultValue={post.days}/>
           <input type="text" name="url" placeholder="Image url of this country" defaultValue={post.url}/>
           <button>Update {post.name}</button>
         </Form>
-        <Form action={`/delete/${post.id}`} method="post">
+          
+        <Form action={`/delete/${post.id}`} method="post" className="form-delete">
           <button>Delete this Country</button>
         </Form>
+
       </div>
-      <Link to="/">
-        <button>Go Back</button>
-      </Link>
+
     </div>
   );
 };
